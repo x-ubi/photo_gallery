2b4371e40b242ce96376121626dc58ea21a2d8c0
Jan Budziński <jan.budzinski.stud@pw.edu.pl>
2020-12-25 17:38:55 +0100

    Implemented Topic class; Figured out how api works (mostly)

--- code_files/fetching_photo.py
+++ code_files/fetching_photo.py
@@ -1,8 +1,30 @@
 import requests
+import json
 
 
-def get_photos_of_selected_topic(url, topic):
-    formatted_url = url.format(id_or_slug=topic)
+def get_topics(url, page_nr):
+    formatted_url = url.format(page_nr=page_nr)
     response = requests.get(formatted_url).json()
-    list_of_photos = [response]
-    return list_of_photos
+    list_of_topics = [Topic(topic) for topic in response]
+    return list_of_topics
+
+
+class Topic:
+    def __init__(self, data):
+        self._id = data['id']
+        self._title = data['title']
+        self._nr_of_photos = data['total_photos']
+
+    def id(self):
+        return self._id
+
+    def title(self):
+        return self._title
+
+    def nr_of_photos(self):
+        return self._nr_of_photos
+
+
+with open("api.json") as file:
+    data = json.load(file)
+    get_topics(data['url'], "1")
