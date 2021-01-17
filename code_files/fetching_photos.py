63826984a12e763da71d4bdc917c57f3f43b9570
Jan Budziński <jan.budzinski.stud@pw.edu.pl>
2021-01-17 04:35:31 +0100

    Refactorized function

--- code_files/fetching_photos.py
+++ code_files/fetching_photos.py
@@ -1,14 +1,10 @@
 import requests
-import json
 
 
-def fetch_photos_of_topic(url, given_id):
-    formatted_url = url.format(given_id=given_id)
-    response = requests.get(formatted_url).json()
+def fetch_photos_of_topic(url, given_id, given_number, given_page):
+    photo_fetching_url = url.format(given_id=given_id,
+                                    given_number=given_number,
+                                    given_page=given_page)
+    response = requests.get(photo_fetching_url).json()
     list_of_photos = [photo for photo in response]
     return list_of_photos
-
-
-with open("api.json") as file:
-    data = json.load(file)
-    photos = fetch_photos_of_topic(data["photo-fetching-url"], "bo8jQKTaE0Y")
