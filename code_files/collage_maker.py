7f1e5395a1b499aa06bbc13eeb96623b0e94837f
Jan Budziński <jan.budzinski.stud@pw.edu.pl>
2021-01-22 21:34:14 +0100

    Remade the function entirely

--- code_files/collage_maker.py
+++ code_files/collage_maker.py
@@ -1,40 +1,50 @@
 from PIL import Image
 import os
+from math import ceil
 
 
 def collage(folder_with_photos,
             name_for_collage,
-            no_of_rows=1,
-            no_of_pics_per_row=[]):
+            no_of_photos=2,
+            no_pics_per_row=1):
     """Create a collage out of given photos.
     Keyword arguments:
     folder_with_photos -- path to a folder with photos inside of it
     name_for_collage -- name for the newly made collage
-    no_of_rows -- number of rows that the collage shall have (default 1)
-    no_of_pics_per_row -- a list of integers indicating how many pictures
-    will be in each of the rows (default [])
+    no_of_photos -- number of photos in the collage (default 2)
+    no_pics_per_row -- number of photos,
+    that the collage will fit in one row (default 1)
     """
     list_of_all_photos = [Image.open(f"{folder_with_photos}/{photo}") for photo
                           in os.listdir(folder_with_photos)]
-    max_height = max(photo.size[1] for photo in list_of_all_photos)
+    max_height = max(photo.height for photo in list_of_all_photos)
     image_width = 1080
-    list_with_grouped_rows = []
-    for row in range(no_of_rows):
-        list_one_row = []
-        for _ in range(no_of_pics_per_row[row]):
-            list_one_row.append(list_of_all_photos[0])
-            list_of_all_photos.pop(0)
-        list_with_grouped_rows.append(list_one_row)
-
+    no_of_rows = ceil(no_of_photos / no_pics_per_row)
     collage_background = Image.new("RGB",
-                                   (max(no_of_pics_per_row)*image_width,
-                                    max_height*no_of_rows))
-    for row in enumerate(list_with_grouped_rows):
-        photo_counter = 0
-        for pic in range(no_of_pics_per_row[row[0]]):
-            photo = row[1][photo_counter]
+                                   (no_pics_per_row * image_width,
+                                    max_height * no_of_rows), "white")
+    list_of_heights = []
+    list_of_photos_sorted_up = sorted(list_of_all_photos,
+                                      key=lambda x: x.height)
+    list_of_photos_sorted_down = sorted(list_of_all_photos,
+                                        key=lambda x: x.height, reverse=True)
+    for _ in range(no_pics_per_row):
+        list_of_heights.append(0)
+    total_pics_counter = 0
+    for row in range(no_of_rows):
+        for pic_no in range(no_pics_per_row):
+            photo = (list_of_photos_sorted_up[0] if row % 2
+                     else list_of_photos_sorted_down[0])
             collage_background.paste(photo,
-                                     (pic*image_width, row[0]*max_height))
-            photo_counter += 1
-    collage_background.show()
+                                     (pic_no*photo.width,
+                                      list_of_heights[pic_no]))
+            list_of_heights[pic_no] += photo.height
+            (list_of_photos_sorted_up.pop(0) if row % 2
+             else list_of_photos_sorted_down.pop(0))
+            total_pics_counter += 1
+            if total_pics_counter == no_of_photos:
+                break
+    collage_background = collage_background.crop((0, 0,
+                                                  no_pics_per_row*image_width,
+                                                  max(list_of_heights)))
     collage_background.save(f"{name_for_collage}.jpg", "JPEG")
